366e6cc65404b60dfe1c326dd79f2910a1bed336
Pete Fowler <fowler.pete@gmail.com>
2022-08-04 20:32:13 -0600

    commit tree b69e7c600a853cbb6daa204b1b7a0a5693a1306b

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  if(x < 400) {
+    return 'This one is on me!';
+  } else if (x < 2000) {
+    return 'That will be twenty bucks.'
+  } else if (x < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(val){
+  switch(val) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
